4e214b6376a5bb65587d3b8f4e168d0a590d344e
Mariya1107 <119432770+Mariya1107@users.noreply.github.com>
2025-06-09 13:37:37 +0530

    Updated settings.py for Render deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,11 +5,11 @@ from datetime import timedelta
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-o+vdt&r+tvb(ehv*ox^(qkw9q7wy)@a6^vvr@!02(o7c8c8c*^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
