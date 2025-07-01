d6f9e4e247dda177cbf165748c8a3005f55484e7
Mariya1107 <119432770+Mariya1107@users.noreply.github.com>
2025-07-02 02:30:32 +0530

    made other changes

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -7,6 +7,23 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+    image1 = serializers.SerializerMethodField()
+    image2 = serializers.SerializerMethodField()
+    image3 = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_image(self, obj):
+        return obj.image.url if obj.image else None
+
+    def get_image1(self, obj):
+        return obj.image1.url if obj.image1 else None
+
+    def get_image2(self, obj):
+        return obj.image2.url if obj.image2 else None
+
+    def get_image3(self, obj):
+        return obj.image3.url if obj.image3 else None
